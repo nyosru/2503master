e9150504b8c50c5449ea234495093e4d046814ec
Sergey <1@php-cat.com>
2025-03-25 06:29:37 +0500

    33

---
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -0,0 +1,54 @@
+<nav class="
+{{--flex flex-1--}}
+{{--justify-end--}}
+">
+    @auth
+        <a
+            href="{{ url('/dashboard') }}"
+            class="
+{{--            rounded-md--}}
+            px-3
+{{--            py-2 --}}
+            text-black ring-1 ring-transparent transition
+            hover:text-black/70
+            focus:outline-none focus-visible:ring-[#FF2D20]
+{{--            dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white--}}
+            "
+        >
+            Поле процессов
+        </a>
+    @else
+        <a
+            href="{{ route('login') }}"
+            class="
+{{--            rounded-md --}}
+            px-3
+{{--            py-2 --}}
+            text-black
+                ring-1 ring-transparent transition
+                hover:text-black/70
+                focus:outline-none focus-visible:ring-[#FF2D20]
+{{--                dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white--}}
+                "
+        >
+            Войти
+        </a>
+
+        @if (Route::has('register'))
+            <a
+                href="{{ route('register') }}"
+                class="
+{{--                rounded-md --}}
+                px-3
+{{--                py-2--}}
+                text-black ring-1 ring-transparent transition
+                hover:text-black/70
+                focus:outline-none focus-visible:ring-[#FF2D20]
+{{--                dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white--}}
+                "
+            >
+                Зарегистрироваться
+            </a>
+        @endif
+    @endauth
+</nav>
