d370405defab0ba1ee2296f487a033778f979a80
Sergey <1@php-cat.com>
2025-03-26 16:20:04 +0500

    11

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('', \App\Livewire\Index::class)->name('enter');
 Route::get('bo', \App\Livewire\Cms2\Leed\LeedBoard::class)->name('leeds');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+//Route::view('dashboard', 'dashboard')
+//    ->middleware(['auth', 'verified'])
+//    ->name('dashboard');
+//
+//Route::view('profile', 'profile')
+//    ->middleware(['auth'])
+//    ->name('profile');
 
 require __DIR__.'/auth.php';
