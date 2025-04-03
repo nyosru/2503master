7a8d1ded93c91e3efd49c500ac0a3a46e3751c59
Сергей <1@php-cat.com>
2025-04-03 13:28:46 +0500

    вход по телеграм

--- resources/views/livewire/app/navigation.blade.php
+++ resources/views/livewire/app/navigation.blade.php
@@ -66,7 +66,7 @@
             </div>
             @else
                 <div class="hidden sm:flex sm:items-center sm:ms-6 space-x-3">
-                    <a href="https://oauth.telegram.org/auth?bot_id={{ env('TELEGRAM_BOT_TOKEN') }}&origin={{ env('APP_URL') }}&return_to={{ env('TELEGRAM_REDIRECT_URL' )}}" >Войти через Telegram</a>
+                    <a href="https://oauth.telegram.org/auth?bot_id={{ env('TELEGRAM_BOT_TOKEN') }}&origin={{ env('APP_URL2') }}&return_to={{ env('TELEGRAM_REDIRECT_URL' )}}" >Войти через Telegram</a>
 {{--                    <a href="{{ route('login') }}" >Вход</a>--}}
 {{--                    <a href="{{ route('register') }}" >Регистрация</a>--}}
                     </div>
