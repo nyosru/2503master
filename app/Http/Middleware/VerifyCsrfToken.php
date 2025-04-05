4ce6fb96f0282f3621ec9f2ad7381b05ac289f76
Сергей <1@php-cat.com>
2025-04-06 01:57:55 +0500

    телега+

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -10,6 +10,9 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 class VerifyCsrfToken extends Middleware
 {
     protected $except = [
-        'webhook', // Путь вашего вебхука
+        '/webhook', // Путь вашего вебхука
+        '/setWebhook', // Путь вашего вебхука
+        '/auth/telegram/callback2', // Путь вашего вебхука
+        '/auth/telegram/callback', // Путь вашего вебхука
     ];
 }
