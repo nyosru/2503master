83970eee6a00500962a3b74241dbcee8674a71ba
Сергей <1@php-cat.com>
2025-04-09 13:37:03 +0500

    122

--- database/migrations/2025_04_02_034944_add_board_id_to_leed_columns_table.php
+++ database/migrations/2025_04_02_034944_add_board_id_to_leed_columns_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
 
             Schema::table('leed_columns', function (Blueprint $table) {
 
-            $table->unsignedBigInteger('board_id');
+//            $table->unsignedBigInteger('board_id');
 
             // Внешние ключи
             $table->foreign('board_id')
