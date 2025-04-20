2478a6751d493778af50fffd48dc28fecb25a9ea
Сергей <1@php-cat.com>
2025-04-20 12:35:39 +0500

    +

--- resources/views/livewire/tech/user-board-role-set-form.blade.php
+++ resources/views/livewire/tech/user-board-role-set-form.blade.php
@@ -6,7 +6,7 @@
     @if( !empty($user->boardUser) )
         @foreach( $user->boardUser as $b )
             {{--                <option>{{$b->board->name}} ({{$b->role->name}})</option>--}}
-            wire:click="setBoardRole({{$user->id ?? 'x'}},{{$b->board->id ?? 'x'}}, {{$b->role->id ?? 'x'}})"
+{{--            wire:click="setBoardRole({{$user->id ?? 'x'}},{{$b->board->id ?? 'x'}}, {{$b->role->id ?? 'x'}})"--}}
             <button
 {{--                wire:click="setBoardRole({{$user->id}},{{$b->board->id}}, {{$b->role->id}})"--}}
                 class="block mb-1 rounded p-1 @if( !empty($b->board->id) && $b->board->id == $user->current_board_id && $b->role->id == $user->roles[0]->id ) bg-blue-500 @else bg-blue-300  @endif "
