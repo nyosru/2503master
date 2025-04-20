4acd1aba9aebd19be3561cf78187b63ebc892965
Сергей <1@php-cat.com>
2025-04-20 12:29:24 +0500

    ап+

--- resources/views/livewire/tech/user-board-role-set-form.blade.php
+++ resources/views/livewire/tech/user-board-role-set-form.blade.php
@@ -9,7 +9,7 @@
             wire:click="setBoardRole({{$user->id ?? 'x'}},{{$b->board->id ?? 'x'}}, {{$b->role->id ?? 'x'}})"
             <button
 {{--                wire:click="setBoardRole({{$user->id}},{{$b->board->id}}, {{$b->role->id}})"--}}
-                class="block mb-1 rounded p-1 @if( $b->board->id == $user->current_board_id && $b->role->id == $user->roles[0]->id ) bg-blue-500 @else bg-blue-300  @endif "
+                class="block mb-1 rounded p-1 @if( !empty($b->board->id) && $b->board->id == $user->current_board_id && $b->role->id == $user->roles[0]->id ) bg-blue-500 @else bg-blue-300  @endif "
             >
                 {{$b->board->name}} ({{$b->role->name}})
             </button>
