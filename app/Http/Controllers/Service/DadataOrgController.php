998b4b851875e4fe8c763dcd950cbd5328821b33
Сергей <1@php-cat.com>
2025-05-18 01:00:51 +0500

    получить инфу о компании по ИНН++

--- app/Http/Controllers/Service/DadataOrgController.php
+++ app/Http/Controllers/Service/DadataOrgController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Service;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class DadataOrgController extends Controller
 {
