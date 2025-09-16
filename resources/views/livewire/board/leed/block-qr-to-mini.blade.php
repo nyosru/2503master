bf9afb96a17ac77075e746911c8c23882b3cfc49
nyosru <nyos@rambler.ru>
2025-09-16 11:06:24 +0500

    ++ доски / шаблоны создавать++

--- resources/views/livewire/board/leed/block-qr-to-mini.blade.php
+++ resources/views/livewire/board/leed/block-qr-to-mini.blade.php
@@ -4,10 +4,10 @@
         <span class="font-bold">QR код Лида</span>
     </div>
     <div class="w-full text-sm max-h-[300px] overflow-auto flex flex-col space-y-1">
+        <a href="{{ $link }}" >
         <img src="{{ $qrCode }}"
              alt="QR Code"
-             class="mx-auto border rounded">
-{{--        <a href="{{ $link }}" >{{ $link }}</a>--}}
+             class="mx-auto border rounded"></a>
     </div>
 
 </div>
