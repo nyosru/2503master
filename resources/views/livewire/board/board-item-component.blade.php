8ef1eaea83550f32335beac7b76e8a83a8f147a1
nyosru <nyos@rambler.ru>
2025-09-28 02:42:50 +0500

    ++ 780

--- resources/views/livewire/board/board-item-component.blade.php
+++ resources/views/livewire/board/board-item-component.blade.php
@@ -41,11 +41,13 @@
                     @php dump($record); @endphp
 
                     @foreach( $record->column->board->fieldSettings as $f )
-{{--                        @if( !empty($record->{$f->field_name}) )--}}
+                        @if( !empty($record->{$f->field_name}) )
 {{--                            {{ $record->{$f->field_name} }}--}}
+                          ee:  {{ $record->{$f->field_name} }}
+                        <br/>
 {{--                            <br/>--}}
 {{--                            @php $hasFields = true; @endphp--}}
-{{--                        @endif--}}
+                        @endif
                     @endforeach
 
                     @if(!$hasFields)
